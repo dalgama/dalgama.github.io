5bde24961f627c93958c4a09d8fb42dc0169eb20
Dilanga Algama <dalga082@uottawa.ca>
2021-05-28 20:11:47 -0400

    Finished Project section

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+function ir() {
+	window.open('https://github.com/dalgama/microblog-information-retrieval-system','mywindow');
+}
+
+
+function webchat() {
+	window.open('https://github.com/dalgama/Webtalky','mywindow');
+}
+
+
+function resto() {
+	window.open('https://github.com/dalgama/restopresto','mywindow');
+}
+
+
+function subway() {
+	window.open('https://github.com/dalgama/ParisMetro','mywindow');
+}
