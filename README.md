1d992dba645d60905ab6feaa92ae2475796a8bbb
Dilanga Algama <dalga082@uottawa.ca>
2021-06-15 10:38:55 -0400

    Finalized desktop layout

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# dalgama.github.io
-My personal Portfolio
+
+Build of my personal Portfolio website.
+
+TODO:
+- Add all the skill.
+- Fit the site layout on mobile.
+- Fit typos.
+- Cleanup.
--- js/index.js
+++ js/index.js
@@ -3,11 +3,17 @@ function ir() {
 	window.open('https://github.com/dalgama/microblog-information-retrieval-system','mywindow');
 }
 
-
 function webchat() {
 	window.open('https://github.com/dalgama/Webtalky','mywindow');
 }
 
+function poker() {
+	window.open('https://github.com/dalgama/AI-poker-game','mywindow');
+}
+
+function bonanza() {
+	window.open('https://github.com/dalgama/BonanzaGame','mywindow');
+}
 
 function resto() {
 	window.open('https://github.com/dalgama/restopresto','mywindow');
